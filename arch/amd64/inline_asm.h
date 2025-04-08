348f87a2f98a99cc1c1410bc737faac9e3f92563
ZHU Yuhui <yuhui.zhu@santannapisa.it>
2025-04-08 03:06:14 +0200

    Oops, now works on AMD Zen4.

---
+++ arch/amd64/inline_asm.h
@@ -0,0 +1,14 @@
+#ifndef __INLINE_ASM_H__
+#define __INLINE_ASM_H__
+
+#define FLUSH_DCACHE(p) asm volatile("clflush (%0)"::"r"(p));
+#define FLUSH_ICACHE(p) asm volatile("clflush (%0)"::"r"(p));
+
+#define NOP(x) asm volatile(".rept " MACRO_TO_STR(x) "\n nop\n .endr")
+#define NOP_PADDING(x) NOP(x)
+
+#define OPS_BARRIER(x)\
+            asm volatile("mfence");\
+            NOP(x);
+
+#endif
--- include/targets.h
+++ include/targets.h
@@ -91,6 +91,16 @@
 #define OPCODE_SIZE_NOP             1
 #define OPCODE_SIZE_RET             1
 #define SIZE_CACHE_STRIDE           256
+// SHOULD BE REMOVED
+#define OPCODE_SIZE                 16
+
+
+#if defined zen4
+#define SZ_BTB_EVSET                (4)
+#define BHB_LEN_IB                  (32)
+// SHOULD BE REMOVED
+#define COND_FP_BITS                (512)
+#endif
 
 #else
 #error "Must specify a valid architecture"
